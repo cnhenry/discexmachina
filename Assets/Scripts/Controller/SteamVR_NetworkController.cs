a55e35c93c368a8d35451e03ad99b1d7c2ec5b8a
Michael Wang <mikewang.6127@gmail.com>
2017-02-12 16:18:17 -0600

    change the name of the network controller script

--- Assets/Scripts/Controller/SteamVR_NetworkController.cs
+++ Assets/Scripts/Controller/SteamVR_NetworkController.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
-using UnityEngine.Networking;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 [RequireComponent(typeof(SteamVR_TrackedObject))]
-public class ControllerInteractionNetwork : NetworkBehaviour {
+public class SteamVR_NetworkController : MonoBehaviour
+{
     public Rigidbody attachPoint;
     public float grabbableRange = 0.05f;
 
@@ -11,13 +12,16 @@ public class ControllerInteractionNetwork : NetworkBehaviour {
     SteamVR_TrackedObject trackedObj;
     FixedJoint joint;
 
-    void Awake() {
+    void Awake()
+    {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         var device = SteamVR_Controller.Input((int)trackedObj.index);
-        if ( joint == null && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger) ) {
+        if (joint == null && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
             // Finds all Objects containing the script "InteractableObject" and puts them all in an
             // array of InteractableObjects.
             // NOTE: GameObject must be accessed through the InteractableObject.
@@ -27,21 +31,25 @@ public class ControllerInteractionNetwork : NetworkBehaviour {
             InteractableObject closest = null;
             float distance = Mathf.Infinity;
             Vector3 position = transform.position;
-            foreach ( InteractableObject io in ios ) {
+            foreach (InteractableObject io in ios)
+            {
                 Vector3 diff = io.transform.position - position;
                 float curDistance = diff.sqrMagnitude;
-                if ( curDistance < distance && curDistance < grabbableRange ) {
+                if (curDistance < distance && curDistance < grabbableRange)
+                {
                     closest = io;
                     distance = curDistance;
                 }
             }
 
-            if ( closest != null ) {
+            if (closest != null)
+            {
                 // Flag InteractableObject as active.
                 interactedObject = closest;
                 interactedObject.beingInteracted = true;
 
-                if ( closest is InteractablePhysicalObject ) { // Create joint and attach to it if closest InteractablePhysicalObject is found.
+                if (closest is InteractablePhysicalObject)
+                { // Create joint and attach to it if closest InteractablePhysicalObject is found.
                     InteractablePhysicalObject ipo = closest as InteractablePhysicalObject;
 
                     ipo.gameObject.transform.position = attachPoint.transform.position + ipo.grabOffsetPosition;
@@ -49,12 +57,16 @@ public class ControllerInteractionNetwork : NetworkBehaviour {
 
                     joint = ipo.gameObject.AddComponent<FixedJoint>();
                     joint.connectedBody = attachPoint;
-                } else if ( closest is InteractableTriggerObject ) { // Trigger the closest found if it is a InteractablePhysicalObject.
+                }
+                else if (closest is InteractableTriggerObject)
+                { // Trigger the closest found if it is a InteractablePhysicalObject.
                     Debug.Log("Detected InteractableTriggerObject");
                 }
             }
-        } else if ( joint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger) ) {
-            if ( interactedObject != null ) // Deactivate InteractableObject if it is one.
+        }
+        else if (joint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if (interactedObject != null) // Deactivate InteractableObject if it is one.
             {
                 interactedObject.beingInteracted = false;
                 var rigidbody = interactedObject.GetComponent<Rigidbody>();
@@ -64,7 +76,8 @@ public class ControllerInteractionNetwork : NetworkBehaviour {
                 float velocityMultiplier = 1.0f;
                 float angularVelocityMultiplier = 1.0f;
 
-                if ( interactedObject is InteractablePhysicalObject ) {
+                if (interactedObject is InteractablePhysicalObject)
+                {
                     InteractablePhysicalObject ipo = interactedObject as InteractablePhysicalObject;
                     velocityMultiplier = ipo.throwMultiplier;
                     angularVelocityMultiplier = ipo.angularThrowMultiplier;
@@ -76,10 +89,13 @@ public class ControllerInteractionNetwork : NetworkBehaviour {
                 // by the same amount we are predicting our render poses.
 
                 var origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
-                if ( origin != null ) {
+                if (origin != null)
+                {
                     rigidbody.velocity = origin.TransformVector(device.velocity) * velocityMultiplier;
                     rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity) * angularVelocityMultiplier;
-                } else {
+                }
+                else
+                {
                     rigidbody.velocity = device.velocity * velocityMultiplier;
                     rigidbody.angularVelocity = device.angularVelocity * angularVelocityMultiplier;
                 }
