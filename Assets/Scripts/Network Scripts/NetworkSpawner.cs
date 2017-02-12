b4875262181f95734731218645c306f2214d85f9
Michael Wang <mikewang.6127@gmail.com>
2017-02-11 22:12:23 -0600

    update the network spawner script
    the disc should spawn with the pressing of the down arrow key

--- Assets/Scripts/Network Scripts/NetworkSpawner.cs
+++ Assets/Scripts/Network Scripts/NetworkSpawner.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using UnityEngine.Networking;
 using UnityEngine;
 
+[RequireComponent(typeof(SteamVR_TrackedObject))]
 public class NetworkSpawner : NetworkBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update () {
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnDisc();
+        }
+
+    }
+
+    [Command]
+    void spawnDisc()
+    {
+        var disc = (GameObject)Instantiate(prefab);
+
+        disc.GetComponent<Rigidbody>().velocity = disc.transform.forward * 6;
+
+        NetworkServer.Spawn(disc);
+    }
+
 }
