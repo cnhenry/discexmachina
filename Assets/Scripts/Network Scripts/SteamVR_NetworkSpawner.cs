f0fad52d8d0a096953aa1cdde37f2c01ca6d4d98
Michael Wang <mikewang.6127@gmail.com>
2017-02-12 14:50:11 -0600

    change the network spawn script with another method

--- Assets/Scripts/Network Scripts/SteamVR_NetworkSpawner.cs
+++ Assets/Scripts/Network Scripts/SteamVR_NetworkSpawner.cs
@@ -7,11 +7,20 @@ public class SteamVR_NetworkSpawner : NetworkBehaviour {
 
     SteamVR_TrackedObject trackedObj;
     public GameObject prefab;
+    public bool triggerButtonDown = false;
+    private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+    private SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
 
     // Use this for initialization
     void Start()
     {
-
+        trackedObj = GetComponent();
     }
 
     // Update is called once per frame
@@ -24,6 +33,13 @@ public class SteamVR_NetworkSpawner : NetworkBehaviour {
         }
 
         var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        triggerButtonDown = controller.GetPressDown(triggerButton);
+        if (triggerButtonDown)
+        {
+            //Debug.Log("Spawn");
+            CmdSpawnDisc();
+        }
 
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger) == true)
         {
