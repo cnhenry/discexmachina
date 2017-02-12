e9d0fea98f0da1d8098c3a62f382fc9c177e41c3
Michael Wang <mikewang.6127@gmail.com>
2017-02-11 22:49:09 -0600

    fix the network spawn script
    now compiles

--- Assets/Scripts/Network Scripts/NetworkSpawner.cs
+++ Assets/Scripts/Network Scripts/NetworkSpawner.cs
@@ -26,13 +26,13 @@ public class NetworkSpawner : NetworkBehaviour {
 
         if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger) == true)
         {
-            spawnDisc();
+            CmdSpawnDisc();
         }
 
     }
 
     [Command]
-    void spawnDisc()
+    void CmdSpawnDisc()
     {
         var disc = (GameObject)Instantiate(prefab);
 
